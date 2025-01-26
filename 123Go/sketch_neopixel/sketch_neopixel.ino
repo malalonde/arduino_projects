6e4fea3e8aeec1dfe1806af9768baaf5125f78d6
malalonde <marcandrelalonde@gmail.com>
2025-01-26 14:37:33 -0500

    123 GO sequence works using auto converted code from makecode

--- 123Go/sketch_neopixel/sketch_neopixel.ino
+++ 123Go/sketch_neopixel/sketch_neopixel.ino
@@ -1,86 +1,79 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 12
+#define PIN 12        // Pin where NeoPixel is connected
+#define NUM_PIXELS 64 // Number of LEDs in the strip
+#define BUTTON_A_PIN 10
+#define BUTTON_B_PIN 11
 
-#define NUMPIXELS 4
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+bool BWasPressed = false;
+uint32_t redColor = strip.Color(50, 0, 0);
+uint32_t greenColor = strip.Color(0, 50, 0);
+uint32_t yellowColor = strip.Color(50, 50, 0);
+uint32_t blackColor = strip.Color(0, 0, 0);
+
+void showRange(uint16_t start, uint16_t length, uint32_t color) {
+    for (uint16_t i = start; i < start + length; i++) {
+        strip.setPixelColor(i, color);
+    }
+    strip.show();
+}
+
+void initialise() {
+    strip.clear();
+    strip.setBrightness(50);
+    strip.show();
+}
+
+void go() {
+    strip.fill(blackColor, 0, NUM_PIXELS);
+    strip.show();
+
+    uint16_t ranges[8] = {0, 8, 16, 24, 32, 40, 48, 56};
+    for (uint8_t i = 0; i < 8; i++) {
+        showRange(ranges[i], 8, redColor);
+        delay(1000);
+    }
+
+    strip.fill(greenColor, 0, NUM_PIXELS);
+    strip.show();
+    delay(2000);
+
+    strip.fill(blackColor, 0, NUM_PIXELS);
+    strip.show();
+}
 
 void setup() {
-  pixels.begin();
+    strip.begin();
+    initialise();
 }
 
 void loop() {
-  pixels.clear();
- for(int i=0; i<NUMPIXELS; i++) 
- {
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.show();
-    delay(500); // Pause before next pass through loop
+//Test Go code
+  static int debugVar = 1;
+  if(debugVar)
+  {
+    debugVar = 0;
+    go();
   }
-}
 
-// input.onButtonPressed(Button.A, function () {
-//     go()
-// })
-// function initialise () {
-//     BWasPressed = 0
-//     range = strip.range(0, 8)
-//     range2 = strip.range(8, 8)
-//     range3 = strip.range(16, 8)
-//     range4 = strip.range(24, 8)
-//     range5 = strip.range(32, 8)
-//     range6 = strip.range(40, 8)
-//     range7 = strip.range(48, 8)
-//     range8 = strip.range(56, 8)
-// }
-// function go () {
-//     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-//     range.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range2.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range3.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range4.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range5.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range6.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range7.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     range8.showColor(neopixel.colors(NeoPixelColors.Red))
-//     basic.pause(1000)
-//     strip.showColor(neopixel.colors(NeoPixelColors.Green))
-//     basic.pause(2000)
-//     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-// }
-// let range8: neopixel.Strip = null
-// let range7: neopixel.Strip = null
-// let range6: neopixel.Strip = null
-// let range5: neopixel.Strip = null
-// let range4: neopixel.Strip = null
-// let range3: neopixel.Strip = null
-// let range2: neopixel.Strip = null
-// let range: neopixel.Strip = null
-// let BWasPressed = 0
-// let strip: neopixel.Strip = null
-// strip = neopixel.create(DigitalPin.P1, 64, NeoPixelMode.RGB)
-// strip.setBrightness(50)
-// initialise()
-// basic.forever(function () {
-//     if (input.buttonIsPressed(Button.B)) {
-//         BWasPressed = 1
-//         strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-//         basic.pause(100)
-//         strip.showColor(neopixel.colors(NeoPixelColors.Black))
-//         basic.pause(100)
-//     } else if (BWasPressed) {
-//         BWasPressed = 0
-//         strip.showColor(neopixel.colors(NeoPixelColors.Red))
-//     } else {
-    	
-//     }
-// })
+    if (digitalRead(BUTTON_A_PIN) == HIGH) { // Replace BUTTON_A_PIN with the pin connected to Button A
+        go();
+    }
+
+    if (digitalRead(BUTTON_B_PIN) == HIGH) { // Replace BUTTON_B_PIN with the pin connected to Button B
+        BWasPressed = true;
+        strip.fill(yellowColor, 0, NUM_PIXELS);
+        strip.show();
+        delay(100);
+        strip.fill(blackColor, 0, NUM_PIXELS);
+        strip.show();
+        delay(100);
+    } else if (BWasPressed) {
+        BWasPressed = false;
+        strip.fill(redColor, 0, NUM_PIXELS);
+        strip.show();
+    }
+}
 
