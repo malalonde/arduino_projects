cf4f4c974b2077c698f74e3038c6d0c1b050a25f
malalonde <marcandrelalonde@gmail.com>
2025-03-23 14:32:50 -0400

    Updated to work with custom PCB

--- CircuitPythonPrototypes/LapCounterPrototype.py
+++ CircuitPythonPrototypes/LapCounterPrototype.py
@@ -1,22 +1,28 @@
+import busio
+import displayio
+import terminalio
+import fourwire
 import board
 import digitalio
-import alarm
 import time
 import busio
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
-import digitalio
 
-button_a = digitalio.DigitalInOut(board.IO27)
+button_a = digitalio.DigitalInOut(board.IO6)
 button_a.direction = digitalio.Direction.INPUT
 button_a.pull = digitalio.Pull.UP
 
-SDA_pin = board.IO26
-SCL_pin = board.IO25
+slot_sensor_a = digitalio.DigitalInOut(board.IO2)
+slot_sensor_a.direction = digitalio.Direction.INPUT
+slot_sensor_a.pull = digitalio.Pull.DOWN
 
-# List I2C devices
+slot_sensor_b = digitalio.DigitalInOut(board.IO4)
+slot_sensor_b.direction = digitalio.Direction.INPUT
+slot_sensor_b.pull = digitalio.Pull.DOWN
+
 def scanI2C():
-    i2c = busio.I2C(SDA_pin, SCL_pin)
+    i2c = busio.I2C(board.IO20, board.IO21)
     i2c.try_lock()
     print(i2c.scan())
     i2c.unlock()
@@ -24,14 +30,14 @@ def scanI2C():
 
 #scanI2C()
 
-# Initialise I2C bus.
-i2c = busio.I2C(board.IO26, board.IO25)  # uses board.SCL and board.SDA
-from lcd.lcd import CursorMode
+SDA_pin = board.IO20
+SCL_pin = board.IO21
+#set update frequency higher to help with slow lcd refresh rate
+i2c = busio.I2C(SDA_pin, SCL_pin)#, frequency=200_000)
 
-# Talk to the LCD at I2C address 0x27.
-# The number of rows and columns defaults to 4x20, so those
-# arguments could be omitted in this case.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=4, num_cols=20)
+lcd = LCD(I2CPCF8574Interface(i2c, 39), num_rows=2, num_cols=16)
+
+print("Starting program.")
 
 lcd.set_cursor_pos(0, 0)
 lcd.print("Hello racers!")
@@ -39,56 +45,81 @@ time.sleep(2)
 lcd.clear()
 
 start_time = time.monotonic()
-last_pressed_time = start_time
-last_lap_duration = last_pressed_time - start_time
-previous_lap_duration = last_lap_duration
+lap_time_a = start_time
+lap_duration_a = lap_time_a - start_time
+lap_time_b = start_time
+lap_duration_b = lap_time_a - start_time
+best_lap_duration = 0.0
 buttonPressed = False
 
-# Configure a PinAlarm to trigger on LOW (object blocking sensor)
-pin_alarm = alarm.pin.PinAlarm(board.IO32, value=True, pull=False)
-future_time = time.monotonic() + 1
-timer_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 1)
-
-print("Monitoring LM393 sensor with PinAlarm...")
-
-def show_lap_time():
-    lcd.set_cursor_pos(0, 0)
-    timeStr = f"Last Lap:{previous_lap_duration}"
-    lcd.print(timeStr)
-    
+def show_best_lap_time(time, track):
+    timeStr = "Best:"
+    if best_lap_duration > 0:
+        timeStr += f"{time:6.3f} ({track})"
+    else:
+        timeStr += "           " #empty space to clear these characters on the LCD
     lcd.set_cursor_pos(1, 0)
-    timeStr = f"Lap: {last_lap_duration}"
     lcd.print(timeStr)
 
-show_lap_time()
+def poll_slot_sensor_until(timeout) -> bool:
+    global lap_duration_a
+    global lap_time_a
+    global lap_duration_b
+    global lap_time_b
+    global best_lap_duration
+    
+    current_time_f = time.monotonic
+    timeout_time = time.monotonic() + timeout
+    counter = 0
+    nothing_in_track_a = False
+    nothing_in_track_b = False
+    while time.monotonic() < timeout_time:
+        counter+=1
+        if slot_sensor_a.value and nothing_in_track_a:
+            lap_duration_a = time.monotonic() - lap_time_a
+            lap_time_a = time.monotonic()
+            lcd.set_cursor_pos(0, 1)
+            lcd.print(f"{lap_duration_a:6.3f}")
+            if(lap_duration_a < best_lap_duration) or best_lap_duration == 0.0:
+                best_lap_duration = lap_duration_a
+                show_best_lap_time(best_lap_duration, "A")
+            return True
+        if not slot_sensor_a.value:
+            nothing_in_track_a = True
+            
+        if slot_sensor_b.value and nothing_in_track_b:
+            lap_duration_b = time.monotonic() - lap_time_b
+            lap_time_b = time.monotonic()
+            lcd.set_cursor_pos(0, 9)
+            lcd.print(f"{lap_duration_b:6.3f}")
+            if(lap_duration_b < best_lap_duration) or best_lap_duration == 0.0:
+                best_lap_duration = lap_duration_b
+                show_best_lap_time(best_lap_duration, "B")
+            return True
+        if not slot_sensor_b.value:
+            nothing_in_track_b = True
+            
+    return False
 
+# Initialise LCD text
+lcd.clear()
+timeStr = f"A{lap_duration_a:6.3f} B{lap_duration_b:6.3f}"
+timeStr += "\nBest:"
+lcd.set_cursor_pos(0, 0)
+lcd.print(timeStr)
+
+# Update loop
 while True:
-    button_click = False
-    # Check if the pin alarm has been triggered
-    timer_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 0.2)
-    alarm.light_sleep_until_alarms(pin_alarm, timer_alarm)
 
     if not button_a.value:  # Button A is pressed
         if not buttonPressed:
+            print("Button Pressed.")
             buttonPressed = True
-            button_click = True
+            best_lap_duration = 0.0
+            show_best_lap_time(best_lap_duration, " ")
     else:
         buttonPressed = False
-    
-    if alarm.wake_alarm == pin_alarm or button_click:
-        print("Object detected! 🚀")  # Fast response when object blocks sensor
-        buttonPressed = True
-        previous_lap_duration = last_lap_duration
-        last_lap_duration = time.monotonic() - last_pressed_time
-        last_pressed_time = time.monotonic()
-        show_lap_time()
 
-    #if alarm.wake_alarm == timer_alarm:
-    #    print("Wake from timer!")
-    
-    #if sensor.value:
-    #    print("Value UP") 
-    
-    # Main program can continue running here (non-blocking)
-    #print("Main loop running...")  
-    #time.sleep(0.1)  # Small delay to prevent CPU overload
+    detected = poll_slot_sensor_until(1)
+    #if detected:
+        #print("Object detected! 🚀")  # Fast response when object blocks sensor
