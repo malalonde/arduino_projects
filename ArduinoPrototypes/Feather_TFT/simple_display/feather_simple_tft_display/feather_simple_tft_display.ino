3757e648d6f6b564c9f41cbc4f536f803d8f0abd
malalonde <marcandrelalonde@gmail.com>
2025-10-18 20:22:19 -0400

    Create feather_simple_tft_display.ino

---
+++ ArduinoPrototypes/Feather_TFT/simple_display/feather_simple_tft_display/feather_simple_tft_display.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+// Use dedicated hardware SPI pins
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print(F("Hello! Feather TFT Test"));
+
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  // Set text size and color
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.setTextSize(4);
+}
+
+int count = 0;
+
+void loop() {
+  tft.setCursor(0, 0);
+  tft.print("Count ");
+  tft.print(count++);
+
+  
+  tft.setCursor(0, 33);
+  tft.print("Line 2");
+}
