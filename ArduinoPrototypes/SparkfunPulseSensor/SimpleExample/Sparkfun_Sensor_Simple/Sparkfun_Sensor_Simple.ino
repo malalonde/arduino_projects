35904de0963f99315522fd2add9ea2993e7357cf
malalonde <marcandrelalonde@gmail.com>
2025-10-26 14:02:11 -0400

    simplify more the simple heart rate sensor code

--- ArduinoPrototypes/SparkfunPulseSensor/SimpleExample/Sparkfun_Sensor_Simple/Sparkfun_Sensor_Simple.ino
+++ ArduinoPrototypes/SparkfunPulseSensor/SimpleExample/Sparkfun_Sensor_Simple/Sparkfun_Sensor_Simple.ino
@@ -11,26 +11,13 @@ SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin);
 bioData body;  
 
 void setup(){
-
   Serial.begin(115200);
 
   Wire.begin();
-  int result = bioHub.begin();
-  if (result == 0) // Zero errors!
-    Serial.println("Sensor started!");
-  else
-    Serial.println("Could not communicate with the sensor!");
+  bioHub.begin();
  
   Serial.println("Configuring Sensor...."); 
-  int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
-  if(error == 0){ // Zero errors!
-    Serial.println("Sensor configured.");
-  }
-  else {
-    Serial.println("Error configuring sensor.");
-    Serial.print("Error: "); 
-    Serial.println(error); 
-  }
+  bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
 
   // Data lags a bit behind the sensor, if you're finger is on the sensor when
   // it's being configured this delay will give some time for the data to catch
