4d27a464ea577d88f1cb8d3fb5258f31d2ef103c
malalonde <marcandrelalonde@gmail.com>
2026-01-24 20:54:27 -0500

    different version of gsr device based on xiao dev board

--- ArduinoPrototypes/Grove_GSR_Sensor/Display/sketch_gsr_display/sketch_gsr_display.ino
+++ ArduinoPrototypes/Grove_GSR_Sensor/Display/sketch_gsr_display/sketch_gsr_display.ino
@@ -3,18 +3,23 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 #include <Wire.h>
- 
-U8G2_SH1107_SEEED_128X128_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
-const int GSR=A3;
+#define USE_U8G2_DISPLAY 0
+
+#if USE_U8G2_DISPLAY //Grove Oled Display
+U8G2_SH1107_SEEED_128X128_1_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+#else // Starter Kit, expansion board, Display
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+#endif
+const int GSR=A0;
 int sensorValue=0;
 int gsr_average=0;
 
 void setup(){
   Serial.begin(9600);
   
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_lubB18_tf );
+  display.begin();
+  display.setFont(u8g2_font_lubB18_tf );
 }
 
 void loop(){
@@ -22,19 +27,22 @@ void loop(){
   for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
       {
       sensorValue=analogRead(GSR);
+      #if defined(ESP32) // If running on esp32, divide by 4 to go from 4095 max reading to 1023
+      sensorValue /= 4;
+      #endif
       sum += sensorValue;
       delay(5);
       }
    gsr_average = sum/10;
    Serial.println(gsr_average);
 
-  u8g2.firstPage();
+  display.firstPage();
   do {
-    u8g2.drawStr(12,32,"GSR:");
+    display.drawStr(12,32,"GSR:");
     char buf[10]; // Buffer must be large enough to hold the number as a string
     sprintf(buf, "%d", gsr_average); // Convert integer to a C-string
-    u8g2.drawStr(12, 64, buf);
-  } while ( u8g2.nextPage() );
+    display.drawStr(12, 64, buf);
+  } while ( display.nextPage() );
 
    delay(100);
 }
